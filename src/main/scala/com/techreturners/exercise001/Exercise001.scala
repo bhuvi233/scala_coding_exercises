1ba0309b14d17e51de2669c31487f9dc60e2202a
Gurunath <bhuvi23389@gmail.com>
2022-02-09 20:09:42 +0000

    Implemented Exercise 1

--- src/main/scala/com/techreturners/exercise001/Exercise001.scala
+++ src/main/scala/com/techreturners/exercise001/Exercise001.scala
@@ -7,13 +7,15 @@ class Exercise001 {
   // we don't specify it in the code
   // https://docs.scala-lang.org/tour/basics.html
 
-  def capitalizeWord(word: String): String = ""
+  def capitalizeWord(word: String): String = word.substring(0,1).toUpperCase() + word.substring(1)
 
-  def generateInitials(firstName: String, lastName: String) = ""
+  def generateInitials(firstName: String, lastName: String) =
+    firstName.substring(0,1) + "." + lastName.substring(0,1)
 
-  def addVat(initialPrice: Double, interestRate: Double) : Double = 0.0
+  def addVat(initialPrice: Double, interestRate: Double) : Double =
+    initialPrice + (((initialPrice * interestRate / 100) * 100 ).round / 100.toDouble )
 
-  def reverse(sentence: String) : String = ""
+  def reverse(sentence: String) : String = sentence.reverse
 
   /**
    * Here we show using a Scala sequence which is a type of collection
@@ -23,8 +25,12 @@ class Exercise001 {
    * @return
    */
   def countLinuxUsers(users: Seq[User]): Int = {
-    // Add your code here
-    0
+    var count = 0
+    if (users != null) {
+      for (user <- users)
+        if (user.osType == "Linux") count += 1
+    }
+    count
   }
 
 }
